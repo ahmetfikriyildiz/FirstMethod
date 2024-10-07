9580712a28b2ca47d6e59c768b3c2b54febb6031
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-07 09:03:59 +0300

    Add project files.

---
+++ FirstMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NameSurname("Ahmet Fikri", "YILDIZ");
+            Add(3,4);
+            RandomGenerator();
+            Returnless();
+        }
+        static void NameSurname(string isim,string soyisim)
+        {
+            Console.WriteLine($"Hoşgeldiniz {isim} {soyisim} ");
+        }
+        static int Add (int x, int y)
+        {
+            int sonuc = x * y;
+            Console.WriteLine("\nÇarpımın sonucu = "+sonuc); 
+            return sonuc;
+        }
+        static int RandomGenerator ()
+        {
+            Random random = new Random();
+            int sonuç = random.Next(0, 100) / 2;
+            Console.WriteLine("\nRandom değer = " + sonuç);
+            return sonuç;
+        }
+        static void Returnless() 
+        {
+            Console.WriteLine("\nKendime Yalan söyledim ...");
+            Console.ReadLine();
+        }
+    }
+}
