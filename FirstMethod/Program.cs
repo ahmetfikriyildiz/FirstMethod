97a721ec190ada7f292ddf705a4a43b16ec5fbc5
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-07 09:07:31 +0300

    .

--- FirstMethod/Program.cs
+++ FirstMethod/Program.cs
@@ -8,6 +8,11 @@ namespace FirstMethod
 {
     internal class Program
     {
+        // Bu Uygulama İçerisinde 4 adet metot yöntemi gösterdim.
+        // 1->Geriye Değer Döndürmeyen Bir void metot.
+        // 2->Geriye Tamsayı Döndüren Bir metot
+        // 3->Parametre Alan ve Geriye Değer Döndüren Bir Metot
+        // 4->Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
         static void Main(string[] args)
         {
             NameSurname("Ahmet Fikri", "YILDIZ");
